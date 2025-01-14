5f92e74dcd988d1f75ccb8ffe55d12435a3b191f
VINICIUS LEANDRO MACEDO <vinicius.lmacedo@senacsp.edu.br>
2025-01-14 15:20:51 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ola sou Vinicius");
+Console.WriteLine("estou aprendendo logica de programação");
---
+++ ex03/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ola, meu nome é Vinicius!");
+Console.WriteLine("Estou estudando no largo treze");
